6cbb081e308bf0560a2c6127fa252a10d258a019
Peter Peto <petoattilapeter98@gmail.com>
2026-01-07 17:16:39 +0100

    Exclude var from tf config

--- infrastructure/provider.tf
+++ infrastructure/provider.tf
@@ -10,7 +10,7 @@ terraform {
   backend "s3" {
     bucket         = "mlops-in-practise-terraform-state"
     key            = "global/s3/terraform.tfstate"
-    region         = var.region
+    region         = "us-west-2"
     dynamodb_table = "mlops-in-practise-terraform-state-locking"
     encrypt        = true
   }
