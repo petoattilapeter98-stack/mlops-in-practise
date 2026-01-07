f6f97d6652933ab21954dec1351f09894f9a37d0
Peter Peto <petoattilapeter98@gmail.com>
2026-01-07 17:15:35 +0100

    Try to init backend for storing state.

--- infrastructure/provider.tf
+++ infrastructure/provider.tf
@@ -6,6 +6,14 @@ terraform {
       version = "~> 5.0"
     }
   }
+
+  backend "s3" {
+    bucket         = "mlops-in-practise-terraform-state"
+    key            = "global/s3/terraform.tfstate"
+    region         = var.region
+    dynamodb_table = "mlops-in-practise-terraform-state-locking"
+    encrypt        = true
+  }
 }
 
 provider "aws" {
