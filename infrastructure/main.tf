4cff730c4f859a324ae2381e8a9a60ab4d44ee21
Peter Peto <petoattilapeter98@gmail.com>
2026-01-07 17:25:37 +0100

    Deploy mlflow bucket.

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,6 +1,6 @@
-# resource "aws_s3_bucket" "this" {
-#   bucket        = local.mlflow_bucket_name
-#   force_destroy = var.force_destroy
+resource "aws_s3_bucket" "this" {
+  bucket        = local.mlflow_bucket_name
+  force_destroy = var.force_destroy
 
-#   tags = var.tags
-# }
+  tags = var.tags
+}
