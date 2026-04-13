5cbb022fab9614ddfd0b9f7e9be3bceee1f1f0db
Peter Peto <petoattilapeter98@gmail.com>
2026-04-13 12:00:54 +0000

    Finalize pullrequest

--- infrastructure/locals.tf
+++ infrastructure/locals.tf
@@ -2,8 +2,6 @@ locals {
   project_name      = "mlops-in-practise"
 
   mlflow_bucket_name = "${local.project_name}-mlflow-artifacts"
-  test_bucket_name = "${local.project_name}-terracotta-ai-test-bucket"
-  
   state_bucket_name = "${local.project_name}-terraform-state"
   dynamodb_name = "${local.project_name}-terraform-state-locking"
 }
